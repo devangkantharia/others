00e1532c385cb849bdd02ee09f52a696007b9e5d
A <devangkantharia@gmail.com>
2023-10-25 15:42:07 +0100

    update

---
+++ src/assets/index.js
@@ -0,0 +1,96 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import link from "./link.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import meta from "./company/meta.png";
+import shopify from "./company/shopify.png";
+import starbucks from "./company/starbucks.png";
+import tesla from "./company/tesla.png";
+
+import ix from "./company/ix.png"
+import xd from "./company/xd.jpg"
+import tcs from "./company/tcs.png"
+import studioif from "./company/studioif.png"
+import tabs from "./company/tabs.png"
+import investis from "./company/investis.png"
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+import vr from "./vr.jpg";
+import ar from "./ar.jpg";
+import mix from "./mix.jpg";
+import touch from "./touch.jpg";
+import arvrmr from "./ar-vr-mr.png";
+import iot from "./iot2.jpg";
+import phygital from "./phygital.jpg";
+import freelance from "./freelance.jpg";
+import exploration from "./exploration.jpg";
+
+export {
+  vr,
+  ar,
+  iot,
+  phygital,
+  mix,
+  touch,
+  arvrmr,
+  freelance,
+  exploration,
+  link,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  shopify,
+  starbucks,
+  tesla,
+  ix,
+  xd,
+  tcs,
+  studioif,
+  tabs,
+  investis,
+  carrent,
+  jobit,
+  tripguide,
+};
